27e32453a5c50e3a1dab34c197087aa39e5f5fab
Hugo <hb255m@hotmail.com>
2024-12-11 03:50:51 -0600

    se arregla

--- canny.py
+++ canny.py
@@ -1,13 +1,19 @@
 #Bibliotecas
 import cv2
-#captura información de la webcam
-#Abrir imagen
-img = cv2.imread('libro.jpg',0)
-#Aplicar canny
-borderCanny = cv2.Canny(img,100,200)
-#Mostrar imagenes
-cv2.imshow('Original', img)
-cv2.imshow('blur', borderCanny)
-#presionar una tecla para ejecutar
-cv2.waitKey(0)
+
+capture = cv2.VideoCapture(0)
+while (capture.isOpened()):
+    ret, frame = capture.read()
+    if (ret == True):
+        #Aplicar canny
+        borderCanny = cv2.Canny(frame,100,200)
+        #Mostrar imagenes
+        cv2.imshow('Original', frame)
+        cv2.imshow('blur', borderCanny)
+        #presionar una tecla para ejecutar
+        if (cv2.waitKey(1) == ord('s')):
+                break
+    else:
+        break
+capture.release()
 cv2.destroyAllWindows()
